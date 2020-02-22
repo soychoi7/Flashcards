ee7abd1beda1d12f62ed4833b34e066a64192dc6
soychoi7 <46826275+soychoi7@users.noreply.github.com>
2020-02-21 22:58:50 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Soyoun Choi on 2/21/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
