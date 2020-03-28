bf603dfed80d67d10dbd07ece9f5e05290101077
soychoi7 <46826275+soychoi7@users.noreply.github.com>
2020-03-28 16:08:09 -0400

    Lab 4 - Final Flashcard App for CodePath iOS App Development Course

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -27,25 +27,52 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //rounding the flashcards
+        card.layer.cornerRadius = 20.0
+        card.clipsToBounds = true
+        /*backLabel.layer.cornerRadius = 20.0
+        backLabel.clipsToBounds = true*/
+        
+        //shadow for the flashcards
+        frontLabel.layer.shadowRadius = 15.0
+        frontLabel.layer.shadowOpacity = 0.5
+        backLabel.layer.shadowRadius = 15.0
+        backLabel.layer.shadowOpacity = 0.5
+        
         
         readSavedFlashcards()
         
         if flashcards.count == 0{
-            updateFlashcard(question: "What is the capital of Florida", answer: "Tallahassee")}else{
-            updateLabels()
+            updateFlashcard(question: "What is the capital of Florida", answer: "Tallahassee")}else{updateLabels()
             updateNextPrevButtons()
     }
     }
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        if(frontLabel.isHidden){frontLabel.isHidden = false; backLabel.isHidden = true} else{frontLabel.isHidden = true;  backLabel.isHidden = false}
-        
+        flipFlashcard()
     }
+    
+    /*func flipFlashcard(){
+
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations: {self.frontLabel.isHidden = true})
+    if(frontLabel.isHidden){frontLabel.isHidden = false; backLabel.isHidden = true} else{frontLabel.isHidden = true;  backLabel.isHidden = false}
+        
+    }*/
+    
+    func flipFlashcard() {
+        UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations: {if self.frontLabel.isHidden == false {self.frontLabel.isHidden = true
+            }
+        else {
+            self.frontLabel.isHidden = false
+            }
+        })
+    }
+
     
     
     func updateLabels(){
         let currentFlashcard = flashcards[currentIndex]
-        
+    
         //Update labels
         frontLabel.text = currentFlashcard.question
         backLabel.text = currentFlashcard.answer
@@ -58,21 +85,45 @@ class ViewController: UIViewController {
         } else{
             nextButton.isEnabled = true
         }
+        if currentIndex == 0{
+            prevButton.isEnabled = false
+        } else {
+            prevButton.isEnabled = true
+        }
     }
     
     @IBOutlet weak var nextButton: UIButton!
     
     @IBOutlet weak var prevButton: UIButton!
+    
+    @IBOutlet weak var card: UIView!
+    
     @IBAction func didTapOnNext(_sender:Any){
         currentIndex = currentIndex+1
-        updateLabels()
+        //updateLabels()
         updateNextPrevButtons()
+        animateCardOut()
     }
     
     @IBAction func didTapOnPrev(_sender:Any){
         currentIndex = currentIndex - 1
         updateLabels()
         updateNextPrevButtons()
+    }
+    
+    func animateCardOut(){
+        UIView.animate(withDuration: 0.3, animations: {self.card.transform = CGAffineTransform.identity.translatedBy(x: -300.00, y: 0.0)}, completion: {finished in
+            //Update labels
+            self.updateLabels()
+            //Run other animation
+            self.animateCardIn()
+        })
+    }
+    
+    func animateCardIn(){
+
+        card.transform = CGAffineTransform.identity.translatedBy(x: 300.00, y: 0.0)
+        UIView.animate(withDuration: 0.3) {self.card.transform = CGAffineTransform.identity}
     }
     
     func saveAllFlashcardsToDisk(){
@@ -152,4 +203,3 @@ class ViewController: UIViewController {
         creationController.flashcardsController = self
     }
 }
-
